564b4c6c4cd7bb9f3649999c7be5a6290b9bd748
Benedikt Eggers <be@spiegelburg.net>
2021-11-26 17:26:31 +0100

    Update AssemblyInfo.cs

--- src/Simplic.DataStack/Properties/AssemblyInfo.cs
+++ src/Simplic.DataStack/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.0.20.328")]
-[assembly: AssemblyFileVersion("7.7.20.328")]
+[assembly: AssemblyVersion("8.0.21.1126")]
+[assembly: AssemblyFileVersion("8.0.21.1126")]
